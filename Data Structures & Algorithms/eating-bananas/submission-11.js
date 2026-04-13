4792afe0e07b2145d90892ddc71a6b6d44389f4f
mboopathi1999 <71706746+mboopathi1999@users.noreply.github.com>
2026-04-13 21:58:34 +0530

    Add: eating-bananas - submission-11

---
+++ Data Structures & Algorithms/eating-bananas/submission-11.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {number[]} piles
+     * @param {number} h
+     * @return {number}
+     */
+    minEatingSpeed(piles, h) {
+        let l = 1
+        let r = Math.max(...piles)
+        let res = r
+        while(l <= r) {
+            const mid = Math.floor((l+r)/2)
+            let totalHours = 0
+            for(let p of piles) {
+                totalHours += Math.ceil(p/mid)
+            }
+            if(totalHours <= h) {
+                res = Math.min(res,mid)
+            }
+            if(totalHours <= h) {
+                r = mid - 1
+            } else {
+                l = mid + 1
+            }
+        }
+        return res
+    }
+}
